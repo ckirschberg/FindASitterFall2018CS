1afc2ce95d91984c3c7e5d135a80c8c797d12d63
ckirschberg@gmail.com <ckirschberg@gmail.com>
2018-09-10 13:48:37 +0200

    loginForm with validators

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,13 +14,14 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
     this.loginForm = this.fb.group(
       {
-        username: ['', Validators.required],
+        username: ['', [Validators.required, Validators.minLength(5)] ],
         password: ['', Validators.required] 
       }
     )
   }
 
   onSubmit(loginForm) {
+    
     if (loginForm.valid) {
       // Send request to back-end to validate login.
     } else {
